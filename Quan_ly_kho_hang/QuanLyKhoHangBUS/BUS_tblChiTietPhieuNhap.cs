8739c3d8836ade37170101d62fd0165874cf51a0
lethitrang <lovely.eyes1803@gmail.com>
2016-11-24 13:38:04 +0700

    them sql phieu nhap, chi tiet phieu nhap

---
+++ Quan_ly_kho_hang/QuanLyKhoHangBUS/BUS_tblChiTietPhieuNhap.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using QuanLyKhoHangDAL;
+using QuanLyKhoHangEntity;
+using System.Data;
+
+namespace QuanLyKhoHangBUS
+{
+    public class BUS_tblChiTietPhieuNhap
+    {
+        SQL_tblChiTietPhieuNhap sql = new SQL_tblChiTietPhieuNhap();
+        public DataTable getALLChiTietPhieuNhap()
+        {
+            return sql.getALLChiTietPhieuNhap();
+        }
+        public DataTable getChitiet_MaPN(string MaPN)
+        {
+            return sql.getChitiet_MaPN(MaPN);
+        }
+        public void Them(EC_tblChiTietPhieuNhap et)
+        {
+            sql.Them(et);
+        }
+        public void Sua(EC_tblChiTietPhieuNhap et)
+        {
+            sql.Sua(et);
+        }
+        public void Xoa(EC_tblChiTietPhieuNhap et)
+        {
+            sql.Xoa(et);
+        }
+        public DataTable getChitiet()
+        {
+            return sql.getChitiet();
+        }
+        public DataTable getChitietHH(string value)
+        {
+            return sql.getChitietHH(value);
+        }
+        public void Them_TT(EC_tblChiTietPhieuNhap et)
+        {
+             sql.Them_TT(et);
+        }
+    }
+}
+
---
+++ Quan_ly_kho_hang/QuanLyKhoHangBUS/BUS_tblPhieuNhap.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using QuanLyKhoHangDAL;
+using QuanLyKhoHangEntity;
+using System.Data;
+
+namespace QuanLyKhoHangBUS
+{
+    public class BUS_tblPhieuNhap
+    {
+        SQL_tblPhieuNhap sql = new SQL_tblPhieuNhap();
+        
+        public DataTable getPhieuNhap()
+        {
+            return sql.getPhieuNhap();
+        }
+        public void Them(EC_tblPhieuNhap et)
+        {
+            sql.Them(et);
+        }
+        public void Sua(EC_tblPhieuNhap et)
+        {
+            sql.Sua(et);
+        }
+        public void Xoa(EC_tblPhieuNhap et)
+        {
+            sql.Xoa(et);
+        }
+        public DataTable getPN(string value)
+        {
+            return sql.getPN(value);
+        }
+        public void CapNhatTongTien()
+        {
+            sql.CapNhatTongTien();
+        }
+        public DataTable LayRaMaPN()
+        {
+            return sql.LayRaMaPN();
+        }
+
+    }
+}
